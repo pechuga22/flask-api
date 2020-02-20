fbabbcbcbc5f9a695923930ea7700491c20aa0cc
pechuga22 <iefaz.santiagovalle@gmail.com>
2020-02-20 17:56:55 -0500

    implement secret key

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
 #tengo que importar las rutas debajo de el app, porque todavia no se como ejecutar tareas en segundo plano haha
 from flaskblog.routes import routes 
 ##---directorio-directorio---archivo
