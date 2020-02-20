fb05c7ea73917f59bde0ea51c091f3a14359bab5
pechuga22 <iefaz.santiagovalle@gmail.com>
2020-02-20 18:30:52 -0500

    template register

--- flaskblog/routes/routes.py
+++ flaskblog/routes/routes.py
@@ -1,5 +1,6 @@
 from flaskblog import app
 from flask import render_template
+from flaskblog.forms import RegistrationForm, LoginForm
 
 @app.route('/')
 def home():
@@ -22,3 +23,8 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title = 'About')    
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return  render_template('register.html', title='Register', form = form)
